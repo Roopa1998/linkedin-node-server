0a761ea94680b294efadf51df2a2ac0ecc0df867
Roopa1998 <30285852+Roopa1998@users.noreply.github.com>
2018-06-23 19:43:26 +0530

    Initial commit

---
+++ OauthParams.js
@@ -0,0 +1,6 @@
+const OauthParams = {
+    client_id: "7834exk3mi0k5b",
+    client_secret: "ZXrEPzrljs5hZMOK",
+    redirect_uri: "https://powerful-bayou-80128.herokuapp.com/auth/linkedin/redirect"
+}
+module.exports = OauthParams;
